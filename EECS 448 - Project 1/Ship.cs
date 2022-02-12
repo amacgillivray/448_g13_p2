689f2a7545a331ed1232b134645af55a304a4dab
Matthew McManness <matthewmcmanness@gmail.com>
2022-02-12 13:58:32 -0600

    added Abstract Ship Class

---
+++ EECS 448 - Project 1/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EECS_448___Project_1
+{
+    internal abstract class Ship
+    {
+        public string Name { get; set; }
+        public string Width { get; set; }
+        public int Hits { get; set; }
+        public OccupationTypes OccupationType { get; set; }
+        public bool IsSunk
+        {
+            get
+            {
+                return Hits >= Width;
+            }
+        }
+    }
+}
