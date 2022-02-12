8178a7f846cfe0d548a5a1a865542aa67a65b9f3
Matthew McManness <matthewmcmanness@gmail.com>
2022-02-12 13:53:42 -0600

    Added Coordinate, Spot, and occupation type classes.

---
+++ EECS 448 - Project 1/OccupationTypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EECS_448___Project_1
+{
+    internal enum OccupationTypes
+    {
+        [Description("O")]
+        Empty,
+
+        [Description("B")]
+        Battleship,
+
+        [Description("R")]
+        Raft,
+
+        [Description("D")]
+        Destroyer,
+
+        [Description("S")]
+        Submarine,
+
+        [Description("A")]
+        Carrier,
+
+        [Description("X")]
+        Hit,
+
+        [Description("M")]
+        Miss
+    }
+}
---
+++ EECS 448 - Project 1/Spot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EECS_448___Project_1
+{
+    internal class Spot
+    {
+        public OccupationType OccupationType { get; set; }
+        public Coordinates Coordinates { get; set; }
+
+        public Spot(int row, int column)
+        {
+            Coordinates = new Coordinates(row, column);
+            OccupationType = OccupationType.Empty;
+        }
+
+        public string Status
+        {
+            get
+            {
+                return OccupationType.GetAttributeOfType<DescriptionAttribute>().Description;
+            }
+        }
+
+        public bool IsOccupied
+        {
+            get
+            {
+                return OccupationType == OccupationType.Battleship
+                    || OccupationType == OccupationType.Destroyer
+                    || OccupationType == OccupationType.Raft
+                    || OccupationType == OccupationType.Submarine
+                    || OccupationType == OccupationType.Carrier;
+            }
+        }
+
+        public bool IsRandomAvailable
+        {
+            get
+            {
+                return (Coordinates.Row % 2 == 0 && Coordinates.Column % 2 == 0)
+                    || (Coordinates.Row % 2 == 1 && Coordinates.Column % 2 == 1);
+            }
+        }
+    }
+}
