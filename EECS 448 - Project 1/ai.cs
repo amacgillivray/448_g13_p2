612e0d0bfa212d56b21222446bf1e2151b23940e
Andrew MacGillivray <agjmacgill@gmail.com>
2022-03-04 14:12:29 -0600

    22.03.04 - Added AI (WIP)

---
+++ EECS 448 - Project 1/ai.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EECS_448___Project_1
+{
+    class AI
+    {
+
+
+        private int rng()
+        {
+            var rand = new Random();
+            return rand.Next(100);
+        }
+
+        private void hitgen_easy()
+        {
+            int x = rng();
+            int y = rng();
+
+            
+        }
+
+        private void hitgen_medium()
+        {
+
+        }
+
+        private void hitgen_hard()
+        {
+
+        }
+
+    }
+}
